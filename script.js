ffdf9ffc445aef890fd841e93dc6f8b4864b334d
Filip Tarajko <filiptarajko@gmail.com>
2021-03-28 19:40:00 +0200

    add winning and losing condition

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ function computerPlay()
 function determineRoundWinner(playerSelection, computerSelection)
 {
     console.log(`Computer's choice: ${computerSelection}, player's choice: ${playerSelection}`)
+    
     if(playerSelection==computerSelection)
     {
         return("tie!");
     }
+    else if(playerSelection == "rock" && computerSelection == "scissors"
+        || playerSelection == "scissors" && computerSelection == "paper"
+        || playerSelection == "paper" && computerSelection == "rock")
+    {
+        return("player wins!");
+    }
+    else if(playerSelection == "scissors" && computerSelection == "rock"
+        || playerSelection == "paper" && computerSelection == "scissors"
+        || playerSelection == "rock" && computerSelection == "paper")
+    {
+        return("computer wins!");
+    }
+
 }
 
 let playerSelection = (window.prompt("Enter your choice: ")).toLowerCase();
