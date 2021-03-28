2a3fbc724b3b4220ae4db4518cbd88c785093050
Filip Tarajko <filiptarajko@gmail.com>
2021-03-28 19:25:00 +0200

    add computerPlay function to return a random option

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("script works!");
+function computerPlay()
+{
+    let options = Array("rock", "paper", "scissors");
+    return options[Math.floor(Math.random()*options.length)];
+}
+
+console.log(computerPlay());
